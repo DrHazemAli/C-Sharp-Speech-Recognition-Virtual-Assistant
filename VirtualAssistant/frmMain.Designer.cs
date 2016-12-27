459cbc3413a22ebaf051c471ef8e35098b47d733
Dr. Hazem Ali <y3m@hotmail.com>
2016-12-28 00:52:26 +0200

    Add files via upload

---
+++ VirtualAssistant/frmMain.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace VirtualAssistant
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnSpeak = new System.Windows.Forms.Button();
+            this.txtChat = new System.Windows.Forms.RichTextBox();
+            this.ProgVoice = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.Image = ((System.Drawing.Image)(resources.GetObject("btnSpeak.Image")));
+            this.btnSpeak.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSpeak.Location = new System.Drawing.Point(88, 307);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(144, 36);
+            this.btnSpeak.TabIndex = 0;
+            this.btnSpeak.Text = "Speak!";
+            this.btnSpeak.UseVisualStyleBackColor = true;
+            this.btnSpeak.Click += new System.EventHandler(this.btnSpeak_Click);
+            // 
+            // txtChat
+            // 
+            this.txtChat.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtChat.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(152)))), ((int)(((byte)(223)))));
+            this.txtChat.Location = new System.Drawing.Point(14, 12);
+            this.txtChat.Name = "txtChat";
+            this.txtChat.Size = new System.Drawing.Size(286, 287);
+            this.txtChat.TabIndex = 1;
+            this.txtChat.Text = "";
+            // 
+            // ProgVoice
+            // 
+            this.ProgVoice.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ProgVoice.Location = new System.Drawing.Point(0, 360);
+            this.ProgVoice.Name = "ProgVoice";
+            this.ProgVoice.Size = new System.Drawing.Size(319, 15);
+            this.ProgVoice.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(319, 375);
+            this.Controls.Add(this.ProgVoice);
+            this.Controls.Add(this.txtChat);
+            this.Controls.Add(this.btnSpeak);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Virtual Assistant";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSpeak;
+        private System.Windows.Forms.RichTextBox txtChat;
+        private System.Windows.Forms.ProgressBar ProgVoice;
+    }
+}
+
---
+++ VirtualAssistant/frmMain.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+namespace VirtualAssistant
+{
+    // ------------------------------------------------------------------//
+    // * Written By Dr. Hazem Ali
+    // * FB : facebook.com/Haz4m
+    // * License : FREE
+    // * SHARING IS SEXY, PLEASE SHARE IT WITH OTHERS, ENJOY =:)
+    // ------------------------------------------------------------------//
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            sre.SpeechRecognized += sre_SpeechRecognized;
+        }
+        SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        DictationGrammar gr = new DictationGrammar();
+        Grammar CustomizedGrammer()
+        {
+
+            var choices = new Choices();
+            //add custom commands
+            choices.Add(File.ReadAllLines(@"Commands.txt"));
+            //to add the letters to the dictionary
+            choices.Add(Enum.GetNames(typeof(Keys)).ToArray());
+
+            var grammer = new Grammar(new GrammarBuilder(choices));
+
+            return grammer;
+        }
+        void Speak(string feedBackText)
+        {
+            if (!String.IsNullOrWhiteSpace(feedBackText))
+            {
+                synthesizer.SpeakAsync(feedBackText);
+                txtChat.AppendText(String.Format("{0} : {1}", "\r\nComputer", feedBackText));
+                
+            }
+            else { Console.WriteLine("feedBack Text is Empty"); }
+        }
+        void getResponse(string orgText)
+        {
+            ProgVoice.Value = 100;
+            txtChat.AppendText("\r\n-------------[ " + DateTime.Now.ToString() + " ]-------------\r\n");
+            txtChat.AppendText("\r\nMe : " + orgText);
+            if (orgText == "hi" || orgText == "hello")
+            {
+                Speak("Hello!");
+            }
+            if (orgText == "what is your name")
+            {
+                Speak("My name is Siri!");
+            }
+            if (orgText == "minimize")
+            {
+                Speak("Alright!");
+                this.WindowState = FormWindowState.Minimized;
+            }
+            if (orgText == "what time is it")
+            {
+                Speak("It's " + DateTime.Now.ToString());
+            }
+
+          
+            Speak("What was that again ?");
+            btnSpeak.Text = "Speak!";
+            txtChat.AppendText("\r\n");
+
+        }
+        void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            try
+            {
+                sre.RecognizeAsyncCancel();
+                sre.RecognizeAsyncStop();
+                getResponse(e.Result.Text.ToLower());
+                btnSpeak.Text = "Speak!";
+                return;
+
+            }
+            catch(Exception er) {
+                Console.WriteLine(er.Message);
+               
+            }
+           
+        }
+        private void btnSpeak_Click(object sender, EventArgs e)
+        {
+            if (btnSpeak.Text == "Speak!")
+            {
+               
+                ProgVoice.Value = 0;
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+                btnSpeak.Text = "Stop!";
+                return;
+            }
+            else
+            {
+                btnSpeak.Text = "Speak!";
+                sre.RecognizeAsyncStop();
+                return;
+            }
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                sre.SetInputToDefaultAudioDevice();
+                sre.LoadGrammar(gr);
+                sre.LoadGrammar(CustomizedGrammer());
+                synthesizer.Volume = 100;  // 0...100
+                synthesizer.Rate = -2;     // -10...10
+
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
